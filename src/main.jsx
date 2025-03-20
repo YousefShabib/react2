526ff09e6f1b725f40ae89c829e622d8629c6274
‪Yousef Shabib‬‏ <s12217432@stu.najah.edu>
2025-03-20 17:48:53 +0200

    teestt

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Navbar from "./components/Navbar/Navbar.jsx";
+import FlightSearch from "./components/FlightSearch/FlightSearch.jsx";
+import Destinations from "./components/Destinations/destinations.jsx";
+import Footer from "./components/Fotter/Fotter.jsx";
+import RecentSearches from "./components/RecentSearches/RecentSearches.jsx";
+import PrepareTrip from "./components/PrepareTrip/PrepareTrip.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Navbar />
+      <FlightSearch/>
+
+
+
+
+
   </StrictMode>,
 )
